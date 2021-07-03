3695918884b6ec0454b41f4959a8c654800a6232
Pawe Szabla <pawelszabla@gmail.com>
2021-07-03 20:23:32 +0200

    This is a test of flask

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", content = "Testing")
+    # return "Hello! this is the main page <h1>HELLO<h1>"
+
+
+
+
+
+
+#
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}"
+#
+#
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("home"))
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)

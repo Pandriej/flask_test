e987b7c27d068148a52e927b883d355f822c73d8
Pawe Szabla <pawelszabla@gmail.com>
2021-07-04 11:33:34 +0200

    part 5

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "test"
+
 
 @app.route("/")
 def home():
-    return render_template("index.html", content = "Testing")
+    return render_template("index.html")
     # return "Hello! this is the main page <h1>HELLO<h1>"
 
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        user = request.form['nm']
+        session['user'] = user
+        return redirect(url_for("user"))
+    else:
+        return render_template("login.html")
 
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
 
 
 

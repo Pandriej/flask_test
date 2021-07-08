595635f4c744a4f2b8dab3b6ce9ad727034ba8c3
Pawe Szabla <pawelszabla@gmail.com>
2021-07-08 20:57:47 +0200

    part10

--- main_old_2.py
+++ main_old_2.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
-from second import second
 
 app = Flask(__name__)
-app.register_blueprint(second, url_prefix="/admin")
 
+@app.route("/home")
 @app.route("/")
-def test():
-    return "<h1> Test </h1>"
-
+def home():
+    return render_template("home.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
